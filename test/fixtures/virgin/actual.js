710a75441168e3b0193906ffc8ad4197fbc7aac9
Douglas Duteil <douglasduteil@gmail.com>
2015-02-19 09:13:04 +0100

    test: can use istanbul ignore

--- test/fixtures/virgin/actual.js
+++ test/fixtures/virgin/actual.js
@@ -1,4 +1,10 @@
 //
+
+var ONE = {};
+
 ONE.foo = function (bar) {
   return baz(bar ? 0 : 1);
 };
+
+/* istanbul ignore next */
+ONE.bar = function (){};
--- test/fixtures/virgin/expectedCover.js
+++ test/fixtures/virgin/expectedCover.js
@@ -1,55 +1,69 @@
 // Compiled to
 
-// 0. "use strict";
-// 1.
-// 2. ONE.foo = function (bar) {
-// 3.   return baz(bar ? 0 : 1);
-// 4. };
-
-
 module.exports = {
   statementMap: [
     {
-      // 2. ONE.foo = function (bar) {
-      // 3.   return baz(bar ? 0 : 1);
-      // 4. };
-      start: {line: 2, column: 0},
-      end: {line: 4, column: 2},
+      // 3. var ONE = {};
+      start: {line: 3, column: 0},
+      end: {line: 3, column: 13},
       skip: undefined
     },
     {
-      // 3.   return baz(bar ? 0 : 1);
-      start: {line: 3, column: 2},
-      end: {line: 3, column: 26},
+      // 5. ONE.foo = function (bar) {
+      // 6.   return baz(bar ? 0 : 1);
+      // 7. };
+      start: {line: 5, column: 0},
+      end: {line: 7, column: 2},
       skip: undefined
+    },
+    {
+      // 6.   return baz(bar ? 0 : 1);
+      start: {line: 6, column: 2},
+      end: {line: 6, column: 26},
+      skip: undefined
+    },
+    {
+      // 10. ONE.bar = function (){};
+      start: {line: 10, column: 0},
+      end: {line: 10, column: 24},
+      skip: true
     }
   ],
 
   fnMap: [
     {
-      // 2. ONE.foo = function (bar) {
-      name: '(anonymous_1)', line: 2,
+      // 5. ONE.foo = function (bar) {
+      name: '(anonymous_1)', line: 5,
       loc: {
-        start: {line: 2, column: 10},
-        end: {line: 2, column: 25}
+        start: {line: 5, column: 10},
+        end: {line: 5, column: 25}
       },
       skip: undefined
+    },
+    {
+      // 10. ONE.bar = function (){};
+      name: '(anonymous_2)', line: 10,
+      loc: {
+        start: {line: 10, column: 10},
+        end: {line: 10, column: 21}
+      },
+      skip: true
     }
   ],
 
   branchMap: [
     {
-      // 3.   return baz(bar ? 0 : 1);
-      line: 3, type: 'cond-expr',
+      // 6.   return baz(bar ? 0 : 1);
+      line: 6, type: 'cond-expr',
       locations: [
         {
-          start: {line: 3, column: 19},
-          end: {line: 3, column: 20},
+          start: {line: 6, column: 19},
+          end: {line: 6, column: 20},
           skip: undefined
         },
         {
-          start: {line: 3, column: 23},
-          end: {line: 3, column: 24},
+          start: {line: 6, column: 23},
+          end: {line: 6, column: 24},
           skip: undefined
         }
       ]

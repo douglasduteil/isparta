4f8b8fc95a655ec398ca012252e3af85a90fa34c
Nathan Cahill <nathan@nathancahill.com>
2015-11-05 17:54:34 -0700

    merge conflicts

--- src/isparta.js
+++ src/isparta.js
@@ -1,3 +1,3 @@
-export * from 'istanbul';
+export {Store, Collector, hook, Report, config, Reporter, utils, matcherFor, Writer, ContentWriter, FileWriter, _yuiLoadHook, TreeSummarizer, assetsDir} from 'istanbul';
 export {Instrumenter} from './instrumenter';
 export {VERSION} from '../package.json';

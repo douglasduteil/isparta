b809b1c3e63038e83cae9b5fbad501e85a1ab922
=?UTF-8?q?Greg=20Berg=C3=A9?= <neoziro@gmail.com>
2015-11-12 10:31:32 +0100

    feat(babel): support babel 6.x.x

--- test/fixtures/es6-classes/compiled.js
+++ test/fixtures/es6-classes/compiled.js
@@ -1,8 +1,12 @@
-"use strict";
+'use strict';
 
-var _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 //
 
@@ -11,36 +15,24 @@ var Animal = (function () {
     _classCallCheck(this, Animal);
   }
 
-  _prototypeProperties(Animal, {
-    getName: {
-      value: function getName() {
-        return "Animal";
-      },
-      writable: true,
-      configurable: true
+  _createClass(Animal, [{
+    key: 'sayHi',
+    value: function sayHi() {
+      return 'Hi, I am a ' + this.type() + '.';
     }
   }, {
-    sayHi: {
-      value: function sayHi() {
-        return "Hi, I am a " + this.type() + ".";
-      },
-      writable: true,
-      configurable: true
-    },
-    sayOther: {
-      value: function sayOther() {
-        return "WAT?!";
-      },
-      writable: true,
-      configurable: true
+    key: 'sayOther',
+    value: function sayOther() {
+      return 'WAT?!';
     }
-  });
+  }], [{
+    key: 'getName',
+    value: function getName() {
+      return 'Animal';
+    }
+  }]);
 
   return Animal;
 })();
 
 exports.Animal = Animal;
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
